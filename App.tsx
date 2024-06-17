563f33e7c0451b41d380f7c1fe24399efffefe75
saadfrhan <saadfarhan347@gmail.com>
2024-06-17 09:35:15 +0500

    flatlist

--- App.tsx
+++ App.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
-import {Image, ScrollView, Text} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const names = [
+  'Devin',
+  'Dan',
+  'Dominic',
+  'Jackson',
+  'James',
+  'Joel',
+  'John',
+  'Jillian',
+  'Jimmy',
+  'Julie',
+];
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={names.map(name => {
+          return {key: name};
+        })}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
 };
 
-const App = () => (
-  <ScrollView>
-    <Text style={{fontSize: 96}}>Scroll me please</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>If you like</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>Scrolling down</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>What's the best</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>Framework around?</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 80}}>React Native</Text>
-  </ScrollView>
-);
-
-export default App;
+export default FlatListBasics;
